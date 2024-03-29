ff7f02cd5d44dab11fd357f14560c44fc3e819ef
FranciIzzi <francesco@dedpartners.com>
2024-03-29 13:50:41 +0100

    "added a plugin for git operations"

---
+++ lua/plugins/git.lua
@@ -0,0 +1,11 @@
+-- NeoGit
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = true,
+}
