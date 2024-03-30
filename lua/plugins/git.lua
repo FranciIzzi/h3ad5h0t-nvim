1e9e9046d5adedc3b3de6441d552e1ef46bf7ac5
FranciIzzi <francesco@dedpartners.com>
2024-03-30 15:11:26 +0100

    "added new functionality to hellp git operations in your project"

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,21 @@
--- NeoGit
+-- Git setup
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
 
-    "nvim-telescope/telescope.nvim", -- optional
-  },
-  config = true,
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+      vim.keymap.set("n","<leader>gp",":Gitsigns preview_hunk<CR>", {})
+      -- vim.keymap.set("n","<leader>gt", ":Gitsign toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
