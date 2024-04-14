5ac366846ae5f0f7998db1d110ac805c381cbeab
FranciIzzi <francesco@dedpartners.com>
2024-04-14 18:04:40 +0200

    "updated some formatting adn lsp config"

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,74 +1,78 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- Setup language servers.
-      local kmap = vim.keymap
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.dartls.setup({
-        cmd = { "dart", "language-server", "--protocol=lsp" },
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.vimls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pylsp.setup({
-        capabilities = capabilities,
-        settings = {
-          pylsp = {
-            plugins = {
-              pycodestyle = {
-                ignore = { "W503", "W391", "W293", "W291" },
-                maxLineLength = 200,
-              },
-            },
-          },
-        },
-      })
-      lspconfig.jinja_lsp.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      kmap.set("n", "K", vim.lsp.buf.hover, {})
-      kmap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      kmap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      kmap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Setup language servers.
+			local kmap = vim.keymap
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.dartls.setup({
+				cmd = { "dart", "language-server", "--protocol=lsp" },
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jamlls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.vimls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+				settings = {
+					pylsp = {
+						plugins = {
+							pycodestyle = {
+								ignore = { "W503", "W391", "W293", "W291" },
+								maxLineLength = 200,
+							},
+						},
+					},
+				},
+			})
+			lspconfig.jinja_lsp.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			kmap.set("n", "K", vim.lsp.buf.hover, {})
+			kmap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			kmap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			kmap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,9 +5,10 @@ return {
 		null_ls.setup({
 			debug = true,
 			sources = {
+        null_ls.builtins.code_actions.refactoring,
+        null_ls.builtins.formatting.biome,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.shellharden,
-				null_ls.builtins.formatting.tsstandard,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.golines,
 				null_ls.builtins.formatting.black,
