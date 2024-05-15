7d003cbe4110608f2bf33ec8ee777f1d093d6706
FranciIzzi <francesco@dedpartners.com>
2024-05-15 21:12:23 +0200

    fixing auto-session

--- lua/h3ad5h0t/plugins/main/session.lua
+++ lua/h3ad5h0t/plugins/main/session.lua
@@ -5,13 +5,6 @@ return {
       log_level = "info",
       auto_session_suppress_dirs = { "~/", "~/Music", "~/Documents", "~/Pictures", "~/Downloads" },
       auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
-      auto_session_use_git_branch = false,
-      bypass_session_save_file_types = false,
-      auto_session_enable_last_session = false,
-      auto_session_create_enabled = false,
-      auto_session_enabled = true,
-      auto_save_enabled = true,
-      auto_restore_enabled = true,
     })
   end,
 }
