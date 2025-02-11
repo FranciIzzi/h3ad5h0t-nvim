961482d363eeff0086e120ee05ba4ff8f65b4705
FranciIzzi <francesco@dedpartners.com>
2025-02-11 23:49:43 +0100

    "Added finder for dotfiles"

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,39 @@
-	return {
-    {
+return {
+	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		event = "VeryLazy",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node_modules",
+						"build",
+						"dist",
+						"git",
+						"yarn.lock",
+					},
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
 			local myKmap = vim.keymap
 			local builtin = require("telescope.builtin")
-			myKmap.set("n", "<leader>ff", builtin.find_files, {desc = "Telescope find file" })
+			myKmap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find file" })
 			myKmap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			myKmap.set("n", "<leader>fw", builtin.grep_string, {})
 			myKmap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
@@ -20,4 +46,3 @@
 		end,
 	},
 }
-
