284f925b237686cb91bef30838b0bd8adece5d2b
FranciIzzi <francesco@dedpartners.com>
2025-07-15 15:53:59 +0200

    "synch del mio config di neovim"

--- lua/plugins/lazy/flutter.lua
+++ lua/plugins/lazy/flutter.lua
@@ -1,11 +1,57 @@
 return {
-	"nvim-flutter/flutter-tools.nvim",
-	lazy = true,
-	ft = "dart",
-	cmd = { "FlutterRun", "FlutterHotReload", "FlutterHotRestart", "FlutterQuit" },
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"stevearc/dressing.nvim",
+	-- {
+	-- 	"mfussenegger/nvim-dap",
+	-- 	dependencies = {
+	-- 		"nvim-neotest/nvim-nio",
+	-- 		"rcarriga/nvim-dap-ui",
+	-- 	},
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		require("dapui").setup({
+	-- 			icons = { expanded = "▾", collapsed = "▸" },
+	-- 			layouts = {
+	-- 				{
+	-- 					elements = {
+	-- 						{ id = "scopes", size = 0.25 },
+	-- 						"breakpoints",
+	-- 						"stacks",
+	-- 						"watches",
+	-- 					},
+	-- 					size = 10,
+	-- 					position = "bottom",
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	{
+		"nvim-flutter/flutter-tools.nvim",
+		lazy = true,
+		ft = "dart",
+		cmd = { "FlutterRun", "FlutterHotReload", "FlutterHotRestart", "FlutterQuit" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim",
+		},
+		config = true,
+		-- config = function()
+		-- require("flutter-tools").setup({
+		-- 	debugger = {
+		-- 		enabled = true,
+		-- 		run_via_dap = true,
+		-- 		exception_breakpoints = {},
+		-- 		register_configurations = function(paths)
+		-- 			local dap = require("dap")
+		-- 			dap.adapters.dart = {
+		-- 				type = "executable",
+		-- 				command = paths.flutter_bin,
+		-- 				args = { "debug-adapter" },
+		-- 			}
+		-- 			dap.configurations.dart = {}
+		-- 			require("dap.ext.vscode").load_launchjs()
+		-- 		end,
+		-- 	},
+		-- })
+		-- end,
 	},
-	config = true,
 }
